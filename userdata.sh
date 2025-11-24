47d0689bba6e5da95402053e51f4fc377823dfec
leoonovaa <144064947+leoonovaa@users.noreply.github.com>
2025-11-24 17:43:47 +0200

    userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,12 +2,9 @@
 
 apt update -y
 
-# Enable GatewayPorts
 sed -i 's/#GatewayPorts no/GatewayPorts yes/' /etc/ssh/sshd_config
 grep -q "GatewayPorts yes" /etc/ssh/sshd_config || echo "GatewayPorts yes" >> /etc/ssh/sshd_config
 
-# Restart SSH
 systemctl restart ssh
 
-# Open reverse-tunnel port (fallback)
 iptables -I INPUT -p tcp --dport 9090 -j ACCEPT
